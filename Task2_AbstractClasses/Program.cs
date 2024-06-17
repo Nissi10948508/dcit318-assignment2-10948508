657691c5640a0a5ac19130b21174cf0565d7b6b7
Nissi <semanyohn@gmail.com>
2024-06-17 11:14:25 +0000

    Implement Task 2: Abstract Classes and Methods

---
+++ Task2_AbstractClasses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
+
+public class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle myCircle = new Circle(4);
+        Console.WriteLine("Area of Circle: " + myCircle.GetArea());
+
+        Rectangle myRectangle = new Rectangle(5, 7);
+        Console.WriteLine("Area of Rectangle: " + myRectangle.GetArea());
+    }
+}
