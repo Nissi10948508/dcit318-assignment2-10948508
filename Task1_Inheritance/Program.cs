82794e3393161450e741f5fe5e515ace54b478e9
Nissi <semanyohn@gmail.com>
2024-06-17 11:11:01 +0000

    Implement Task 1: Inheritance and Method Overriding

---
+++ Task1_Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        myAnimal.MakeSound();
+
+        Dog myDog = new Dog();
+        myDog.MakeSound();
+
+        Cat myCat = new Cat();
+        myCat.MakeSound();
+    }
+}
+
